2a55d8f08738d04994729ab7aa04ea349ae5c045
daylonr <daylonk.rogers@gmail.com>
2015-03-31 14:55:25 -0500

    Main docs
    simple_html_dom is used in reference for the file_get_html function to
    work

---
+++ BD.php
@@ -0,0 +1,56 @@
+<?php
+set_time_limit(0);
+include 'simple_html_dom.php';
+$bookDepot = file_get_html('http://www.bookdepository.com/category/3149/Bibles/');
+if (isset($bookDepot)) {
+	foreach ($bookDepot->find('h3') as $h3) {
+		foreach ($h3->find('a') as $aElement) {
+			$bookLink = $aElement->href;
+			$content[0] = $bookLink;
+		}
+		// echo 'Book Link: '; #needs to be json identifier
+		// echo $bookLink.'<br>'; #needs to be json info
+		$indBook = file_get_html($bookLink);
+		foreach ($indBook->find('h1') as $bookTitle) {
+			$bookTitle = $bookTitle->plaintext;
+			$content[1] = $bookTitle;
+		// echo 'Book Title: '; #needs to be json identifier
+		// echo $bookTitle.', '; #needs to be json info
+		}
+		foreach ($indBook->find('img') as $bigImg) {
+			if ($bookTitle == $bigImg->alt) {
+				$bigImg = $bigImg->src;
+				$content[2] = $bigImg;
+				// echo 'Big Image: '; #needs to be json identifier
+				// echo $bigImg.', '; #needs to be json info
+			}
+		}
+		foreach ($indBook->find('ul') as $ul) {
+			$ulClass = $ul->class;
+			if ($ulClass == "biblio") {
+				foreach ($ul->find('li') as $li) {
+					$plainLi = $li->plaintext;
+					$content[3] = $plainLi;
+					// echo $plainLi.', ';
+				}
+			}
+		}
+		foreach ($indBook->find('div') as $pDiv) {
+			$divClass = $pDiv->class;
+			if ($divClass == "longDescription") {
+				foreach ($pDiv->find('p') as $para) {
+					$para = $para->plaintext;
+					$content[4] = $para;
+					// echo 'Long Description: '; #needs to be json identifier
+					// echo $para; #needs to be json info
+				}
+			}
+		}
+	}
+	file_put_contents("BookDepot.json", json_encode($content, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | FILE_APPEND));
+}
+# What I need to print in the JSON
+	#$smallImg, $bigImg
+#Completed things to print out
+	#$bookLink
+?>
